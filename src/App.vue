8b33fcae97047a0ed31e849b67860298f99bd4ce
waterlin <a10311036@gmail.com>
2022-03-10 23:21:15 +0800

    chore: init app.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import liff from "@line/liff";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    this.initLiff();
+  },
+  methods: {
+    initLiff() {
+      liff
+        .init({ liffId: process.env.VUE_APP_LIFFID })
+        .then(() => {
+          console.log("Success! you can do something with LIFF API here.");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
